055bc7fd3e67cc1e36c6b3766154e8a9805192ba
Nick Dunn <nickdunn92@icloud.com>
2024-05-10 09:53:23 +0100

    deleted card component, changed style of navbar and tachyons styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,48 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = ({ name }) => {
   return (
-    <header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-      <nav class="f6 fw6 ttu tracked">
-        <a class="link dim white dib mr3" href="#" title="Home">Home</a>
-        <a class="link dim white dib mr3" href="#" title="About">About</a>
-        <a class="link dim white dib mr3" href="#" title="Store">Store</a>
-        <a class="link dim white dib" href="#" title="Contact">Contact</a>
-      </nav>
-    </header>
+    <div>
+      <header 
+        className="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l"
+        >
+        <nav 
+          className="f6 fw6 ttu tracked"
+          >
+          <a 
+            className="fl w-10 link dim white dib mr3" href="#" title="Home"
+            >
+              Home
+            </a>
+          <a 
+            className="fl w-10 link dim white dib mr3" href="#" title="About"
+            >
+              About
+            </a>
+          <a 
+            className="fl w-10 link dim white dib mr3" href="#" title="Store"
+            >
+              Store
+            </a>
+          <a 
+            className="fl w-10 link dim white dib" href="#" title="Contact"
+            >
+              Contact
+            </a>
+        <div
+          className="fl w-10 link white dib"
+          >
+          <h1>
+            {name}
+          </h1>
+          <img
+            alt='user profile'
+            src='https://i.etsystatic.com/33269334/r/il/c0beb2/3719088381/il_570xN.3719088381_sco7.jpg'
+            />
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import { user } from './User';
 import 'tachyons';
 import Navbar from './Navbar';
@@ -9,8 +8,7 @@ import Navbar from './Navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <Card name={user.name} />
+    <Navbar name={user.name}/>
   </React.StrictMode>
 );
 
