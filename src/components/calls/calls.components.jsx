e02d9d35f75039113c891946fb45897a6b6882c8
unknown <nickdunn92@icloud.com>
2024-11-18 13:14:18 +0000

    changed call component to functional component

--- src/components/calls/calls.components.jsx
+++ src/components/calls/calls.components.jsx
@@ -1,11 +1,9 @@
-import { Component } from React;
 
-class Calls extends Component {
-  render () {
-    return (
-      <h1>Calls</h1>
-    );
-  }
-}
+
+const Calls = () => {
+  return (
+    <h1>Calls</h1>
+  );
+};
 
 export default Calls;
